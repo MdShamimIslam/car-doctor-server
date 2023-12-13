43fa50c2b5b663e9de1d4239c316b18b8ccfb2f3
MdShamimIslam <shamim401897@gmail.com>
2023-12-13 19:11:44 +0600

    booking collection created and all work implement

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require ('express');
 const app = express();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 const port = process.env.PORT || 5000;
 const cors = require('cors');
@@ -25,11 +25,58 @@ async function run() {
     await client.connect();
 
     const serviceCollection = client.db('geniusDb').collection('services');
+    const bookingCollection = client.db('geniusDb').collection('bookings');
 
-
+    // service related api
     app.get('/services', async(req,res)=>{
         const result = await serviceCollection.find().toArray();
         res.send(result);
+    })
+
+    app.get('/services/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)};
+      const options = {
+        projection: { title:1, img:1, price:1, service_id:1 },
+      };
+      const result = await serviceCollection.findOne(query,options);
+      res.send(result);
+    })
+
+    // bookings related api
+    app.post('/bookings', async(req,res)=>{
+      const booking = req.body;
+      const result = await bookingCollection.insertOne(booking);
+      res.send(result);
+    })
+
+    app.get('/bookings', async(req,res)=>{
+      let query = {};
+      if(req.query?.email){
+        query = { email : req.query.email}
+      };
+      const result = await bookingCollection.find(query).toArray();
+      res.send(result);
+    })
+
+    app.delete('/bookings/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = { _id : new ObjectId(id)};
+      const result = await bookingCollection.deleteOne(query);
+      res.send(result);
+    })
+
+    app.patch('/bookings/:id', async(req,res)=>{
+      const id = req.params.id;
+      const filter = { _id:new ObjectId(id) };
+      const updateBooking = req.body;
+      const updateDoc = {
+        $set: {
+          status: updateBooking.status
+        },
+      };
+      const result = await bookingCollection.updateOne(filter,updateDoc)
+      res.send(result);
     })
 
 
